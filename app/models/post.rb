5c5ae689320f454f90fed4d58ddb263a913dc1f3
Angel Rosario <arosario.us@gmail.com>
2022-03-16 16:42:52 -0400

    commit tree 92351c5186055b4ffbe94af72de7ce1a31149859

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :clickbait
+
+  GOODTITLES = ["Won't Believe", "Secret", "Top [#{number}]", "Guess"]
+
+  def clickbait
+    # byebug 
+    unless title.match?(/GOODTITLES/)
+      errors.add(:title, "Not enough clickbait") 
+    end
+  end
+
 end
